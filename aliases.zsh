84f273ff8810e0b3994cfc7cbba6a9af4cce5bf3
Suresh Ramaswamy <suresh.ramaswamy@nsadmins-MacBook-Pro.local>
2018-06-08 18:10:20 +0530

    added more aliases

--- aliases.zsh
+++ aliases.zsh
@@ -2,10 +2,17 @@
 alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
 alias reloadcli="source $HOME/.zshrc"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
-alias ll="$(brew --prefix coreutils)/libexec/gnubin/ls -ahlF --color --group-directories-first"
-weather() { curl -4 wttr.in/${1:-antwerp} }
-alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
+weather() { curl -4 wttr.in/${1:-bangalore} }
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
+alias c="clear"
+alias p="ps -fu $LOGNAME | grep -v grep"
+alias grep="grep -i"
+alias whoelse="who | grep -v $USER"
+alias ff="find . -name \!* -print"
+alias lc="tr '[A-Z]' '[a-z]'"
+alias uc="tr '[a-z]' '[A-Z]'"
+alias spellit="echo \!* | spell"
+alias log="\!* | tee \!:1.log"
 
 # Directories
 alias dotfiles="cd $DOTFILES"
@@ -16,3 +23,11 @@ alias sites="cd $HOME/Sites"
 alias gst="git status"
 alias gd="git diff"
 alias gl="git log --oneline --decorate --color"
+
+# override the default ls aliases
+alias lsa='ls -lah'
+alias l='ls -lAh'
+alias la='ls -lah'
+alias ll="$(brew --prefix coreutils)/libexec/gnubin/ls -ahlF --color --group-directories-first"
+
+
